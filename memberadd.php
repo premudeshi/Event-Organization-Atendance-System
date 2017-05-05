fb2e2cfbfeebd32f233e45457be7201a9dd77527
Prem Udeshi <premudeshi99@gmail.com>
2017-05-05 19:28:58 +0300

    Member Adding Complete
    Now you can successfully Add a member to the database using this simple
    system.

---
+++ memberadd.php
@@ -0,0 +1,24 @@
+
+<?php 
+include_once "navbar.php";
+?>
+
+<html>
+
+<form action="addmember.php" method="post">
+  Name:<br>
+  <input type="text" name="name"><br>
+  ICAI Number:<br>
+  <input type="Number" name="idnumber"> <br>
+    Email:<br>
+  <input type="email" name="email"> <br>
+  <input type="submit" value="Submit">
+
+
+</form>
+
+
+</html>
+
+
+
